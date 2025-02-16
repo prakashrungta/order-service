b55babf5ded7f3a36dc6ecbd087cec46bc9989b2
Prakash Rungta <contact.prakash.rungta@gmail.com>
2025-02-16 11:55:03 +0530

    Working Code

--- src/test/java/com/foodapp/order/OrderServiceApplicationTests.java
+++ src/test/java/com/foodapp/order/OrderServiceApplicationTests.java
@@ -1,3 +1,5 @@
+package com.foodapp.order;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
--- src/test/java/com/foodapp/order/entity/OrderTest.java
+++ src/test/java/com/foodapp/order/entity/OrderTest.java
@@ -1,8 +1,6 @@
-package com.foodapp.customer.entity;
+package com.foodapp.order.entity;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
